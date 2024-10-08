b5cd8c8365518e403d91beb487e649487d48c619
R4nz5r <ragibshahriar43@gmail.com>
2024-10-08 17:09:02 +0600

    Home Done

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import "./App.css"
 
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
       {/* Other components */}
+      <Home/>
     </BrowserRouter>
   );
 };
